53d522e444ce9acfb826c285020b09fab916cb7f
munirapp <softwaremakassar@gmail.com>
2020-02-29 20:58:00 +0800

    Setup Profile Create Project

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,13 +1,15 @@
 export const createProject = project => {
   return (dispatch, getState, { getFirestore }) => {
     const firestore = getFirestore();
+    const profile = getState().firebase.profile;
+    const authId = getState().firebase.auth.uid;
     firestore
       .collection("projects")
       .add({
         ...project,
-        authorFirstName: "Munir",
-        authorLastName: "AP",
-        authorId: 12345,
+        authorFirstName: profile.firstName,
+        authorLastName: profile.lastName,
+        authorId: authId,
         createdAt: new Date()
       })
       .then(() => {
